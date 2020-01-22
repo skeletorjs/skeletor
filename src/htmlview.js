29a6d8f707076e865133b8f36f07c76ba4b4b582
JC Brand <jc@opkode.com>
2020-01-22 19:04:51 +0100

    Add HTMLView

---
+++ src/htmlview.js
@@ -0,0 +1,16 @@
+import { isFunction, isNil } from "lodash";
+import { View } from "./view";
+import { render } from 'lit-html';
+import tovnode from "tovnode";
+
+
+export const HTMLView = View.extend({
+
+    render () {
+        debugger;
+        isFunction(this.beforeRender) && this.beforeRender();
+        render(this.toHTML(), this.el);
+        isFunction(this.afterRender) && this.afterRender();
+        return this;
+    }
+});
